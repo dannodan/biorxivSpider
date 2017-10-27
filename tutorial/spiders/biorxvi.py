7bf15d0c9353ee54b23271529e9bc4f3d2a0e437
dannodan <dannodan89@gmail.com>
2017-10-27 02:02:31 -0400

    Added new fields to scrapping

--- tutorial/spiders/biorxvi.py
+++ tutorial/spiders/biorxvi.py
@@ -16,11 +16,12 @@ class BiorxivSpider(scrapy.Spider):
             "title": response.css(".highwire-cite-title::text").extract_first(),
             "authors": self.processAuthors(response),
             "doi": response.css(".highwire-cite-metadata-doi::text").extract_first(),
-            "abstract": response.css(".highwire-markup .abstract #p-2::text").extract_first()
+            "abstract": response.css(".highwire-markup .abstract #p-2::text").extract_first(),
+            "link": response.css(".pane-biorxiv-art-tools .panel-region-content-left .inside .pane-custom a::attr(href)").extract_first(),
+            "date": response.css(".pane-biorxiv-publication-history .published span::text").extract_first() + response.css(".pane-biorxiv-publication-history .published::text").extract_first()
         }
 
     def processAuthors(self, response):
-
         author_list = []
         for author in response.css(".main-content-wrapper .highwire-citation-author"):
             author_name = author.css(".nlm-given-names::text").extract_first()
@@ -35,8 +36,8 @@ class BiorxivSpider(scrapy.Spider):
                 inner_info = response.urljoin(inner_info)
                 yield scrapy.Request(inner_info, callback=self.getInfo)
 
-        next_page = response.css(".pager-wrapper .page-group-last .pager-next a::attr(href)").extract_first()
+        # next_page = response.css(".pager-wrapper .page-group-last .pager-next a::attr(href)").extract_first()
 
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+        # if next_page is not None:
+        #     next_page = response.urljoin(next_page)
+        #     yield scrapy.Request(next_page, callback=self.parse)
